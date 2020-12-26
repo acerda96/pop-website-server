a876fe7049473a19090583a80f821cbc4b898a11
Alex Cerda <alex.cerda@nurole.com>
2020-12-26 12:20:59 +0000

    Fix sign up validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -6,6 +6,7 @@ const registerSchema = Joi.object({
   email: Joi.string().min(6).required().email(),
   name: Joi.string().min(1).required(),
   password: Joi.string().min(6).required(),
+  mobileNumber: Joi.string().min(1).required(),
   confirmPassword: Joi.string().min(6).required(),
 });
 
