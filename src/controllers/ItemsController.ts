c0b7336daa8134a47509a19d589333931247bdb6
Alex Cerda <alex.cerda@nurole.com>
2020-12-28 15:58:03 +0000

    Remove nearest first

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -6,8 +6,6 @@ import upload from "../utils/multerUpload";
 import Joi from "@hapi/joi";
 import splitCamelCase from "../utils/helpers";
 
-// const RESULTS_PER_PAGE = 20;
-
 const itemSchema = Joi.object({
   name: Joi.string().min(1).required(),
   description: Joi.string().min(1).required(),
@@ -119,42 +117,21 @@ router.put("/:id", verifyToken, upload.array("images", 4), (req, res) => {
 });
 
 router.get("/", async (req, res) => {
+  const RESULTS_PER_PAGE = 20;
   const storeId = req.query.storeId;
-  const findTerm = storeId ? { storeId } : { status: "approved" };
-  const latitude = Number(req.query.latitude);
-  const longitude = Number(req.query.longitude);
-
   const sortCriterion: number = Number(req.query.sortCriterion);
-  // const page = Number(req.query.page) || 0;
-  // const size = Number(req.query.size) || RESULTS_PER_PAGE;
+  const page = Number(req.query.page) || 0;
+  const size = Number(req.query.size) || RESULTS_PER_PAGE;
 
+  const findTerm = storeId ? { storeId } : { status: "approved" };
   const sortTerm: { $natural?: number; price?: number } =
-    sortCriterion === 0
-      ? { $natural: -1 }
-      : sortCriterion === 1
-      ? { price: 1 }
-      : {};
+    sortCriterion === 0 ? { $natural: -1 } : { price: 1 };
 
   try {
-    const items = await Item.find(findTerm).sort(sortTerm);
-    // .skip(RESULTS_PER_PAGE * page)
-    // .limit(size);
-
-    if (
-      sortCriterion === 0 ||
-      (sortCriterion === 3 && (!longitude || !latitude))
-    ) {
-      items.forEach(async (item: any) => {
-        const store: any = await Store.findById(item.storeId);
-        item.distance = distance(
-          latitude,
-          longitude,
-          store.position.lat,
-          store.position.lng
-        );
-      });
-      items.sort(compare);
-    }
+    const items = await Item.find(findTerm)
+      .sort(sortTerm)
+      .skip(RESULTS_PER_PAGE * page)
+      .limit(size);
 
     return res.status(200).json(items);
   } catch (err) {
@@ -162,34 +139,5 @@ router.get("/", async (req, res) => {
     return res.status(404).json({ error: "Items could not be retrieved" });
   }
 });
-
-function compare(a, b) {
-  if (a.distance < b.distance) {
-    return -1;
-  }
-  if (a.distance > b.distance) {
-    return 1;
-  }
-  return 0;
-}
-
-function distance(lat1, lon1, lat2, lon2) {
-  const radlat1 = (Math.PI * lat1) / 180;
-  const radlat2 = (Math.PI * lat2) / 180;
-  const theta = lon1 - lon2;
-  const radtheta = (Math.PI * theta) / 180;
-  let dist =
-    Math.sin(radlat1) * Math.sin(radlat2) +
-    Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-  if (dist > 1) {
-    dist = 1;
-  }
-  dist = Math.acos(dist);
-  dist = (dist * 180) / Math.PI;
-  dist = dist * 60 * 1.1515;
-  dist = dist * 1.609344; // get in KM
-
-  return dist;
-}
 
 export default router;
